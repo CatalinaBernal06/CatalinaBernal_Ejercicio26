645f9d3189c5deaaad63a88611520940dc5d4792
CatalinaBernal06 <37335457+CatalinaBernal06@users.noreply.github.com>
2018-05-10 07:52:00 -0500

    par

---
+++ parabolico.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+p= 100000
+v0 = np.random.uniform(35, 45, p)
+g = 9.8
+datos = [[56, 66], [109, 119], [26, 36], [172, 182]]
+
+v1 = v0[cond]
+
+plt.figure()
+plt.hist(out)
+plt.show()
+
+for dato  in datos:
+    d = ((v0**2)*np.sin(2*th0))/g
+    cond = (d<dato[1]) & (d>dato[0])
+    th0 = np.random.uniform(0, np.pi/2, p)
+  
+    
+    
+
+
+
+
+
+
+
+
+
